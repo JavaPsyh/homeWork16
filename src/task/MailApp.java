42435b41320d8e05f938fe82ed3a898304f05a01
JavaPsyh <psyhjava@gmail.com>
2019-06-26 20:54:57 +0300

    initial commit

---
+++ src/task/MailApp.java
@@ -0,0 +1,17 @@
+package task;
+
+public class MailApp {
+
+    public static class MailMessage {
+        // implement here
+    }
+
+    public static class Salary {
+        // implement here
+    }
+
+    public static class MailService {
+        // implement here
+    }
+
+}
